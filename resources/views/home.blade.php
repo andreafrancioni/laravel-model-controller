39af534d0ea8c6c437c846437798c6f3194995a0
andreafrancioni <andreafrancioni.work@gmail.com>
2024-01-22 18:27:08 +0100

    crea card in pagina e assegna stile scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,25 @@
 
 @section('content')
 
-<ul>
+<h1 class="text-center">MOVIES</h1>
+
+<div class="cardWrapper container d-flex align-items-center flex-wrap justify-content-center">
+
     @foreach ($movies as $movie)
-    <li>{{$movie}}</li>
+    <div class="card">
+        <div class="cardTitle d-flex flex-column align-items-center">
+            <h3>{{$movie->title}}</h3>
+            <h6>{{$movie->original_title}}</h6>
+        </div>
+        <div class="cardDescription d-flex flex-column align-items-center">
+            <span>{{$movie->nationality}}</span>
+            <span>{{$movie->date}}</span>
+            <span>{{$movie->vote}}</span>
+        </div>
+    </div>
     @endforeach
-</ul>
+    
+</div>
 
 @endsection
+
